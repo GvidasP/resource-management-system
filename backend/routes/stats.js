17428676db47c3aea308d426feed093c56db8e89
Gvidas Pranauskas <gvidaspran@gmail.com>
2020-03-27 22:47:31 +0200

    Writing statistics of spools into mongodb

--- backend/routes/stats.js
+++ backend/routes/stats.js
@@ -7,40 +7,30 @@ const db = mongoose.connection;
 //Get all info about different values
 router.get("/", async (req, res) => {
     try {
-        db.collection("statistics").findOne(
-            {},
-            { sort: { _id: -1 } },
-            (err, data) => {
-                res.json(data);
-            }
-        );
+        const doc = await db
+            .collection("statistics")
+            .findOne({}, { sort: { _id: -1 } });
+        res.status(200).json(doc);
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
 });
 
 router.post("/update", async (req, res) => {
-    const update = {
-        manufacturers: req.body.manufacturers
-    };
-    // db.collection("statistics")
-    //     .findOneAndUpdate(
-    //         { sort: { _id: -1 } },
-    //         { $set: update },
-    //         {
-    //             returnNewDocument: true
-    //         }
-    //     )
-    //     .then(updatedDoc => console.log(updatedDoc))
-    //     .catch(error => console.log(error));
-    // res.status(200).json(data);
-    db.collection("statistics").findOne(
-        {},
-        { sort: { _id: -1 } },
-        (err, data) => {
-            db.collection("statistics").insertOne();
-        }
-    );
+    try {
+        const oldDoc = await db
+            .collection("statistics")
+            .findOne({}, { sort: { _id: -1 } });
+        const docUpdate = {
+            $set: {
+                manufacturers: [...oldDoc.manufacturers, req.body.manufacturer]
+            }
+        };
+        db.collection("statistics").updateOne({ _id: oldDoc._id }, docUpdate);
+        res.status(200).json(docUpdate);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 });
 
 module.exports = router;
